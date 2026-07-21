ec168ca01bb878443dea75bcffbeba45bcf1f472
Sahil Shinde <sahilshinde352007@gmail.com>
2026-07-21 13:42:02 +0530

    Add files via upload

---
+++ Assignment-2/Dynamic_Report_Generator.py
@@ -0,0 +1,72 @@
+def report_header(func):
+    def wrapper(*args, **kwargs):
+        print("=" * 40)
+        print("         STUDENT REPORT")
+        print("=" * 40)
+        func(*args, **kwargs)
+        print("=" * 40)
+    return wrapper
+
+
+class Report:
+    college = "MIT ADT University, Pune"
+
+    def __init__(self, name, roll, marks):
+        self.name = name
+        self.roll = roll
+        self.marks = marks
+
+    @classmethod
+    def change_college(cls, new_name):
+        cls.college = new_name
+
+    def __str__(self):
+        return (f"Name     : {self.name}\n"
+                f"Roll No  : {self.roll}\n"
+                f"Marks    : {self.marks}")
+
+    @report_header
+    def display_report(self):
+        print(f"College  : {Report.college}")
+        print(self)
+
+        if self.marks >= 40:
+            print("Result   : PASS")
+        else:
+            print("Result   : FAIL")
+
+
+
+name = input("Enter Student Name: ")
+roll = int(input("Enter Roll Number: "))
+marks = int(input("Enter Marks: "))
+
+
+student = Report(name, roll, marks)
+
+
+student.display_report()
+
+
+choice = input("\nDo you want to change the college name? (yes/no): ")
+
+if choice.lower() == "yes":
+    new_college = input("Enter New College Name: ")
+    Report.change_college(new_college)
+
+    print("\nUpdated Report:")
+    student.display_report()
+
+#Output
+#Enter Student Name: sahil
+#Enter Roll Number: 18
+#Enter Marks: 50
+#========================================
+#         STUDENT REPORT
+#========================================
+#College  : MIT ADT University, Pune
+#Name     : sahil
+#Roll No  : 18
+#Marks    : 50
+#Result   : PASS
+#========================================
